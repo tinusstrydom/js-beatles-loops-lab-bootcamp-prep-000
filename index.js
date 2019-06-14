8607f75f816e9ce6f158090074fd6aed788a28fb
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-14 19:38:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for(let i = 0; i < musicians.length; i++){
