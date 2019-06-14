74cd9bbc3da2746dbee48328d79dc61fc1049cab
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-14 20:09:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments){
-  var array = [];
+  let array = [];
   for(let i = 0; i < musicians.length; i++){
     array.push(musicians[i] + " plays " + instruments[i]);
   }
+  return array;
+}
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(){
+  var array = [];
+	var i = 0;
+	while(i < facts.length){
+		console.log(facts[i] + "!!!");
+		i++;
+	}
   return array;
 }
