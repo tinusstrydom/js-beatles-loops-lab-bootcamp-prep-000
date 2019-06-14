fe10c94b74337a0cc92bb6987a33f68600f311a4
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-14 19:36:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for(let i = 0; i < musicians.length; i++){
-    array.push(musicians[i] + " plays" + instruments[i]); 
+    array.push(musicians[i] + " plays" + instruments[i]);
   }
 
-  return array;  
+  return array;
 
 }
