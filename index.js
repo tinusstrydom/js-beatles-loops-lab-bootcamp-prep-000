7af1b8b6010333af7846c74a3435f7046286cdf5
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-14 19:38:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments){
   var array = [];
   for(let i = 0; i < musicians.length; i++){
-    array.push(musicians[i] + " plays" + instruments[i]);
+    array.push(musicians[i] + " plays " + instruments[i]);
   }
 
   return array;
